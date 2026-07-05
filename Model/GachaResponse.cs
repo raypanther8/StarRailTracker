8558f03b44f5e91ec12f0f83c74e7ce5078d5d3e
raypanther8 <raypanther8@gmail.com>
2026-07-05 16:55:28 +0800

    加入專案檔案。

---
+++ Model/GachaResponse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StarRailTracker.Model
+{
+    public class GachaLog
+    {
+        [JsonPropertyName("uid")]
+        public int Uid { get; set; }
+
+        [JsonPropertyName("gacha_id")]
+        public int GachaId { get; set; }
+
+        [JsonPropertyName("gacha_type")]
+        public int GachaType { get; set; }
+
+        [JsonPropertyName("item_id")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lang")]
+        public string Lang { get; set; }
+
+        [JsonPropertyName("item_type")]
+        public string ItemType { get; set; }
+
+        [JsonPropertyName("rank_type")]
+        public int RankType { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; } // 改用 string 完美避開 BigInteger 限制
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("list")]
+        public List<GachaLog> List { get; set; }
+
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("size")]
+        public int Size { get; set; }
+    }
+
+    public class GachaResponse
+    {
+        [JsonPropertyName("retcode")]
+        public int Retcode { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StarRailTracker.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("=== 星鐵抽卡追蹤器 ===");
+
+HttpClient client = new();
+var dejson_options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true, // 忽略大小寫
+    NumberHandling = JsonNumberHandling.AllowReadingFromString// 允許將JSON string轉換成int/bool
+};
+string url = Console.ReadLine();
+
+try
+{
+    string json_result = await client.GetStringAsync(url);
+    GachaResponse gachaResponse = JsonSerializer.Deserialize<GachaResponse>(json_result, dejson_options);
+    List<GachaLog> gachaLogs = gachaResponse.Data.List;
+
+    var Warps = gachaLogs.DistinctBy(x => x.Id).ToList();
+    Warps = Warps.OrderByDescending(x => x.Time).ToList();
+
+    Console.WriteLine($"總抽卡數量: {Warps.Count}");
+    Console.WriteLine("=== 抽卡紀錄 ===");
+    foreach ( GachaLog log in Warps)
+    {
+        Console.WriteLine($"名稱: {log.Name}, {log.RankType}★ {log.ItemType}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"發生錯誤: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("=== 程式結束 ===");
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
